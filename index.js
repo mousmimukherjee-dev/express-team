d2900ff79e936c592133169bbcda4fa8b9213e07
SgnCycles <signe.bekere@gmail.com>
2026-02-09 17:36:01 +0100

    add footer structure

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express"
 
 import * as path from "path"
+import "dotenv/config"
 
 const app = express()
 const port = process.env.PORT
 const __dirname = path.resolve()
 
-app.use(express.static("public"))
 app.set("view engine", "ejs")
+
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
   res.render(path.join(__dirname, "/views/pages/index"),
@@ -18,4 +20,4 @@ app.get("/", (req, res) => {
 )
 })
 
-app.listen(port, () => console.log(`Listening on port ${PORT}`))
+app.listen(port, () => console.log(`Listening on port ${port}`))
