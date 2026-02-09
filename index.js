c021b641249f20969694f506bdf667f87c2de2af
Villiam Agnas <villiamagnas@gmail.com>
2026-02-09 13:04:37 +0100

    initializing index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-alert("Helloooo!");
+import express from "express"
+
+import * as path from "path"
+
+const app = express()
+const port = process.env.PORT
+const __dirname = path.resolve()
+
+app.use(express.static("public"))
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+  res.render(path.join(__dirname, "/views/pages/index"),
+  {
+
+  }
+
+)
+})
+
+app.listen(port, () => console.log(`Listening on port ${PORT}`))
