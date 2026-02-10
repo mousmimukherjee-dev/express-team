257f75ec9fda196895def35350c381700815540b
SgnCycles <signe.bekere@gmail.com>
2026-02-10 17:41:34 +0100

    add home & about page content and style

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 import express from 'express';
-import teamMembers from './data/team_member_details.js';
+import aboutRouter from './routes/about.js';
+import { homepageDescription } from './data/team_member_details.js';
 import * as path from 'path';
 import 'dotenv/config';
 
-const app = express()
+const app = express();
 const port = process.env.PORT;
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
 
-app.use(express.static("public"))
+app.use(express.static("public"));
+app.use('/about', aboutRouter);
 
 app.get("/", (req, res) => {
   res.render(path.join(__dirname, "/views/pages/index"),
   {
     headTitle: "home",
+    homepageContent: homepageDescription,
 
   }
 
