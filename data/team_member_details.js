217b30820153bf69d3c93a3c19e0e1e020801089
Busanz <buddhisanz@gmail.com>
2026-02-10 16:16:01 +0530

    Create data file

---
+++ data/team_member_details.js
@@ -0,0 +1,40 @@
+const teamMembers = [
+  {
+    id: 1,
+    name: 'Mousumi',
+    role: 'Frontend Developer',
+    bio: 'Loves building responsive and accessible web interfaces.',
+    image: '/images/team/mousumi.jpg',
+    email: 'mark@example.com',
+    linkedin: 'https://linkedin.com/in/Mousumi',
+  },
+  {
+    id: 2,
+    name: 'Signe',
+    role: 'Frontend Developer',
+    bio: 'Ensures projects are delivered on time and on point.',
+    image: '/images/team/signe.jpg',
+    email: 'sara@example.com',
+    linkedin: 'https://linkedin.com/in/signe',
+  },
+  {
+    id: 3,
+    name: 'Villiam',
+    role: 'Frontend Developer',
+    bio: 'Ensures projects are delivered on time and on point.',
+    image: '/images/team/villiam.jpg',
+    email: 'sara@example.com',
+    linkedin: 'https://linkedin.com/in/villiam',
+  },
+  {
+    id: 4,
+    name: 'Buddhi Sandaruwan',
+    role: 'UI/UX Designer and Frontend Developer',
+    bio: 'Passionate about creating intuitive user experiences.',
+    image: '/images/team/alice.jpg',
+    email: 'alice@example.com',
+    linkedin: 'https://linkedin.com/in/alicejohnson',
+  },
+];
+
+export default teamMembers;
--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-import express from "express"
+import express from 'express';
+import teamMembers from './data/team_member_details.js';
+import * as path from 'path';
+import 'dotenv/config';
 
-import * as path from "path"
+const app = express();
+const port = process.env.PORT;
+const __dirname = path.resolve();
 
-const app = express()
-const port = process.env.PORT
-const __dirname = path.resolve()
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-app.use(express.static("public"))
-app.set("view engine", "ejs")
+app.get('/', (req, res) => {
+  res.render(path.join(__dirname, '/views/pages/index.ejs'), {});
+});
+app.get('/team', (req, res) => {
+  res.render('team', { teamMembers });
+});
 
-app.get("/", (req, res) => {
-  res.render(path.join(__dirname, "/views/pages/index"),
-  {
-
-  }
-
-)
-})
-
-app.listen(port, () => console.log(`Listening on port ${PORT}`))
+app.listen(port, () => console.log(`Listening on port ${port}`));
